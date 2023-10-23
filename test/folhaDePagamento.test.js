b7207c0224bb32e7ddfaa3dfe6a9edd1f5a96550
Vitor Hugo Migoto de Gouvêa <56165095+vitormigoto@users.noreply.github.com>
2023-10-23 01:41:38 +0000

    Alterando os test para it

--- test/folhaDePagamento.test.js
+++ test/folhaDePagamento.test.js
@@ -1,14 +1,14 @@
 import { somaHorasExtras, calculaDescontos } from '../index.js';
 
 describe('Testes das funcoes de Folha de Pagamento', () => {
-  test('Deve retornar a soma das Horas Extras', () => {
+  it('Deve retornar a soma das Horas Extras', () => {
     const esperado = 2500;
     const retornado = somaHorasExtras(2000, 500);
 
     expect(retornado).toBe(esperado);
   });
 
-  test('Deve descontar o valor do salario', () => {
+  it('Deve descontar o valor do salario', () => {
     const esperado = 2300;
     const retornado = calculaDescontos(2500, 200);
 
