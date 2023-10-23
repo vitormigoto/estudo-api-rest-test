d4d8179308206d61667f826a75becc41806ddab4
Vitor Hugo Migoto de Gouvêa <56165095+vitormigoto@users.noreply.github.com>
2023-10-23 01:41:08 +0000

    Adicionando descrição de agrupamento de testes

--- test/folhaDePagamento.test.js
+++ test/folhaDePagamento.test.js
@@ -1,15 +1,17 @@
 import { somaHorasExtras, calculaDescontos } from '../index.js';
 
-test('Deve retornar a soma das Horas Extras', () => {
-  const esperado = 2500;
-  const retornado = somaHorasExtras(2000, 500);
+describe('Testes das funcoes de Folha de Pagamento', () => {
+  test('Deve retornar a soma das Horas Extras', () => {
+    const esperado = 2500;
+    const retornado = somaHorasExtras(2000, 500);
 
-  expect(retornado).toBe(esperado);
-});
+    expect(retornado).toBe(esperado);
+  });
 
-test('Deve descontar o valor do salario', () => {
-  const esperado = 2300;
-  const retornado = calculaDescontos(2500, 200);
+  test('Deve descontar o valor do salario', () => {
+    const esperado = 2300;
+    const retornado = calculaDescontos(2500, 200);
 
-  expect(retornado).toBe(esperado);
+    expect(retornado).toBe(esperado);
+  });
 });
