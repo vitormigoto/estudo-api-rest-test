055b7de65fa3e26f5f54a9f24ba95bd6ff5b1641
Vitor Hugo Migoto de Gouvêa <56165095+vitormigoto@users.noreply.github.com>
2023-11-03 13:07:52 +0000

    test: adicionando teste para validar exceptions

--- carrinho/test/carrinho.test.js
+++ carrinho/test/carrinho.test.js
@@ -29,4 +29,13 @@ describe('Testes do Carrinho', () => {
 
     expect(carrinho).toHaveProperty('total');
   });
+
+  it('Deve lançar erro ao finalizar compra com carrinho vazio', () => {
+    function englobaErroCarrinho() {
+      const carrinho = new Carrinho();
+      carrinho.finalizaCompra();
+    }
+
+    expect(englobaErroCarrinho).toThrowError('Carrinho de compras vazio');
+  });
 });
