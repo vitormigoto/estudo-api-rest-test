5c86dc0d0f979c03e4985870051770b9e91cf4a2
Vitor Hugo Migoto de Gouvêa <56165095+vitormigoto@users.noreply.github.com>
2023-11-03 12:58:24 +0000

    test: adicionando teste para validar se carrinho possui os itens adicionados

--- carrinho/test/carrinho.test.js
+++ carrinho/test/carrinho.test.js
@@ -7,4 +7,20 @@ describe('Testes do Carrinho', () => {
 
     expect(carrinho.subtotal).toBeNull();
   });
+
+  it('Deve ter itens', () => {
+    const item = new Item('Banana', 2, 5);
+    const item2 = new Item('Maça', 0.5, 1);
+
+    const carrinho = new Carrinho();
+    carrinho.adiciona(item);
+    carrinho.adiciona(item2);
+
+    expect(typeof carrinho).toBe('object');
+    expect(carrinho.itens[0]).toBe(item);
+    expect(carrinho.itens[1]).toBe(item2);
+
+    expect(carrinho.itens).toContain(item);
+    expect(carrinho.itens).toContain(item2);
+  });
 });
