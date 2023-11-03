eadb383b3d13c2be71886186a5815dd686ddc1d2
Vitor Hugo Migoto de Gouvêa <56165095+vitormigoto@users.noreply.github.com>
2023-11-03 12:44:33 +0000

    test: adicionando teste do valor total do item com toBeCloseTo

--- carrinho/test/item.test.js
+++ carrinho/test/item.test.js
@@ -8,4 +8,10 @@ describe('Testes dos itens', () => {
     expect(item.valor).toBe(2.5);
     expect(item.quantidade).toBe(10);
   });
+
+  it('Deve ter o preço calculado de acordo com a quantidade', () => {
+    const item = new Item('Batata', 0.1, 3);
+
+    expect(item.pegaValorTotalItem()).toBeCloseTo(0.3);
+  });
 });
