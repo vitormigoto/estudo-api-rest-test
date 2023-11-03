e70e540ac59a05d4cb42f64f3347b96e3bf44a25
Vitor Hugo Migoto de Gouvêa <56165095+vitormigoto@users.noreply.github.com>
2023-11-03 15:38:37 +0000

    test: Adicionando teste de verificacao se o item esta no banco de dados

--- testes-parte2/src/test/models/editora.test.js
+++ testes-parte2/src/test/models/editora.test.js
@@ -1,4 +1,4 @@
-import {describe, expect, it} from '@jest/globals';
+import { describe, expect, it } from '@jest/globals';
 import Editora from '../../models/editora';
 
 describe('Testando o modelo Editora', () => {
@@ -13,6 +13,31 @@ describe('Testando o modelo Editora', () => {
 
     expect(editora).toEqual(
       expect.objectContaining(objetoEditora),
+    );
+  });
+
+  it.skip('Deve salvar editora no BD', () => {
+    const editora = new Editora(objetoEditora);
+
+    editora.salvar().then((dados) => {
+      expect(dados.nome).toBe('CDC');
+    });
+  });
+
+  it('Deve salvar no BD usando sintaxe moderna', async () => {
+    const editora = new Editora(objetoEditora);
+
+    const dados = await editora.salvar();
+
+    const retornado = await Editora.pegarPeloId(dados.id);
+
+    expect(retornado).toEqual(
+      expect.objectContaining({
+        id: expect.any(Number),
+        ...objetoEditora,
+        created_at: expect.any(String),
+        updated_at: expect.any(String),
+      }),
     );
   });
 });
