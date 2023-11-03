59ea4fd188b8451c6d1d42b1c20a4b6c9ed33870
Vitor Hugo Migoto de Gouvêa <56165095+vitormigoto@users.noreply.github.com>
2023-11-03 13:48:18 +0000

    fix: Ajustando os diretorios do estudo

---
+++ testes-parte1/carrinho/test/carrinho.test.js
@@ -0,0 +1,76 @@
+import Carrinho from '../carrinho';
+import Item from '../item';
+
+describe('Testes do Carrinho', () => {
+  it('Deve inicializar vazio', () => {
+    const carrinho = new Carrinho();
+
+    expect(carrinho.subtotal).toBeNull();
+  });
+
+  it('Deve ter itens', () => {
+    const item = new Item('Banana', 2, 5);
+    const item2 = new Item('Maça', 0.5, 1);
+
+    const carrinho = new Carrinho();
+    carrinho.adiciona(item);
+    carrinho.adiciona(item2);
+
+    expect(typeof carrinho).toBe('object');
+    expect(carrinho.itens[0]).toBe(item);
+    expect(carrinho.itens[1]).toBe(item2);
+
+    expect(carrinho.itens).toContain(item);
+    expect(carrinho.itens).toContain(item2);
+  });
+
+  it('Deve ter a propriedade "total" na inicialização', () => {
+    const carrinho = new Carrinho();
+
+    expect(carrinho).toHaveProperty('total');
+  });
+
+  it('Deve lançar erro ao finalizar compra com carrinho vazio', () => {
+    function englobaErroCarrinho() {
+      const carrinho = new Carrinho();
+      carrinho.finalizaCompra();
+    }
+
+    expect(englobaErroCarrinho).toThrowError('Carrinho de compras vazio');
+  });
+
+  it('Deve adicionar o frete', () => {
+    const carrinho = new Carrinho();
+    carrinho.adicionaFrete(10);
+
+    expect(carrinho.frete).toBe(10);
+  });
+
+  it('Deve finalizar com sucesso a compra', () => {
+    const item = new Item('Banana', 2, 5);
+    const item2 = new Item('Mel', 1, 5);
+
+    const carrinho = new Carrinho();
+    carrinho.adiciona(item);
+    carrinho.adiciona(item2);
+    carrinho.adicionaFrete(10);
+
+    expect(carrinho.finalizaCompra()).toStrictEqual({
+      subtotal: 15,
+      frete: 10,
+      total: 25,
+    });
+  });
+
+  it('Deve trazer o total dos itens com Frete', () => {
+    const item = new Item('Banana', 3, 5);
+    const item2 = new Item('Mel', 4, 5);
+
+    const carrinho = new Carrinho();
+    carrinho.adiciona(item);
+    carrinho.adiciona(item2);
+    carrinho.adicionaFrete(5);
+
+    expect(carrinho.calculaTotal()).toBe(40);
+  });
+});
